d399fcb3c2d5a2bb0789491c46eb3c5d2531ff0a
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-10-10 18:20:33 +0200

    add Hero text

--- frontend/tech-haven/src/components/Hero.jsx
+++ frontend/tech-haven/src/components/Hero.jsx
@@ -1,4 +1,16 @@
+import { Link } from 'react-router-dom'
+import './Hero.css'
+
 const Hero = () => {
-  return <section className="hero">Hero</section>
+  return (
+    <section className="hero">
+      <div className="hero__left">
+        <h1 className="hero__title">Make your life a tech haven</h1>
+        <Link to="/products" className="hero__cta">
+          Shop Now
+        </Link>
+      </div>
+    </section>
+  )
 }
 export default Hero
