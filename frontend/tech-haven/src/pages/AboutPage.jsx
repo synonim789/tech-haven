f7307a653cab86426964bd912448f247a8fedf0e
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-10-12 17:26:08 +0200

    Add AboutPage

--- frontend/tech-haven/src/pages/AboutPage.jsx
+++ frontend/tech-haven/src/pages/AboutPage.jsx
@@ -1,4 +1,29 @@
+import './AboutPage.css'
+import image from '../assets/gadgets.jpg'
 const AboutPage = () => {
-  return <div>AboutPage</div>
+  return (
+    <main className="about-us">
+      <div className="about-us__left">
+        <h1 className="about-us__title">About us</h1>
+        <p className="about-us__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ab
+          aliquam quod? Architecto adipisci neque eaque necessitatibus ducimus
+          quod molestiae, minima quis repudiandae sint ex eligendi illo ratione
+          libero consectetur! Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Sequi, magnam tenetur! Veritatis corrupti voluptatum saepe
+          aspernatur. Perspiciatis mollitia ea iure, ut molestiae adipisci.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ab
+          aliquam quod? Architecto adipisci neque eaque necessitatibus ducimus
+          quod molestiae, minima quis repudiandae sint ex eligendi illo ratione
+          libero consectetur! Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Sequi, magnam tenetur! Veritatis corrupti voluptatum saepe
+          aspernatur. Perspiciatis mollitia ea iure, ut molestiae adipisci.
+        </p>
+      </div>
+      <div className="about-us__right">
+        <img src={image} alt="" className="about-us__image" />
+      </div>
+    </main>
+  )
 }
 export default AboutPage
