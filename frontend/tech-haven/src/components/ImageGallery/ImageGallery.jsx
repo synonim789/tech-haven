f87ce78596a7726abd2da29946c5fabe03e2ed9c
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-10-19 17:10:11 +0200

    Style Image Gallery

---
+++ frontend/tech-haven/src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import './ImageGallery.css'
+
+const ImageGallery = ({ images = [''] }) => {
+  const [main, setMain] = useState(images[0])
+  useEffect(() => {
+    setMain(images[0])
+  }, [images])
+
+  return (
+    <div className="image-gallery">
+      <div className="image-gallery__main">
+        <img src={main} alt="" />
+      </div>
+      <div className="image-gallery__gallery">
+        {images.map((image, index) => {
+          return (
+            <img
+              src={image}
+              alt=""
+              key={index}
+              onClick={() => setMain(images[index])}
+              className={image === main ? 'image-gallery__active' : ''}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+export default ImageGallery
