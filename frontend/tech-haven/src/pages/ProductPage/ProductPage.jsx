d3b8180f115d19355513dae3b6790a0ac13fd2f3
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-10-19 17:10:45 +0200

    Style Single Product Page

--- frontend/tech-haven/src/pages/ProductPage/ProductPage.jsx
+++ frontend/tech-haven/src/pages/ProductPage/ProductPage.jsx
@@ -1,8 +1,12 @@
 import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import ImageGallery from '../../components/ImageGallery/ImageGallery'
 import { useProductsContext } from '../../context/products_context'
-
+import './ProductPage.css'
+import { AiFillStar } from 'react-icons/ai'
+import AddToCart from '../../components/AddToCart/AddToCart'
 const ProductPage = () => {
+  const navigate = useNavigate()
   const { id } = useParams()
   const {
     singleProduct: product,
@@ -14,6 +18,13 @@ const ProductPage = () => {
     getSingleProduct(`http://localhost:3000/api/v1/products/${id}`)
   }, [id])
 
+  if (loading) {
+    console.log('Loading')
+  }
+
+  if (error) {
+    console.log('Error')
+  }
   const {
     name,
     description,
@@ -27,15 +38,32 @@ const ProductPage = () => {
   } = product
 
   return (
-    <div>
-      <img src={image} alt={name} />
-      <h1>{name}</h1>
-      <p>{description}</p>
-      <p>{brand}</p>
-      <h3>${price}</h3>
-      <p>{rating}</p>
-      <button>Add To Cart</button>
-      <p> - Amount: 1 +</p>
+    <div className="product-page">
+      <div className="product-page__left">
+        <p onClick={() => navigate(-1)} className="product-page__cta">
+          Go Back
+        </p>
+        <ImageGallery images={images} />
+      </div>
+      <div className="product-page__right">
+        <p className="product-page__rating">
+          <AiFillStar />
+          {rating}
+        </p>
+        <h1 className="product-page__title">{name}</h1>
+        <p className="product-page__description">{description}</p>
+        <p className="product-page__brand">
+          <span>Brand: </span>
+          {brand}
+        </p>
+        <p className="product-page__available">
+          <span>Available: </span>
+          {countInStock > 0 ? 'In Stock' : 'Not in Stock'}
+        </p>
+        <h3 className="product-page__price">${price}</h3>
+
+        <AddToCart />
+      </div>
     </div>
   )
 }
