e069814b34d23a87e1ba454c321bba92f40000b5
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-10-23 13:59:16 +0200

    Add ProductsPage basic Styling

--- frontend/tech-haven/src/pages/ProductsPage/ProductsPage.jsx
+++ frontend/tech-haven/src/pages/ProductsPage/ProductsPage.jsx
@@ -1,4 +1,28 @@
+import SingleProduct from '../../components/SingleProduct/SingleProduct'
+import { useProductsContext } from '../../context/products_context'
+import './ProductsPage.css'
+
 const ProductsPage = () => {
-  return <div>ProductsPage</div>
+  const { products } = useProductsContext()
+  console.log(products)
+  return (
+    <section className="products-page">
+      <input
+        type="text"
+        placeholder="search"
+        className="products-page__search"
+      />
+      <div className="products-page__content">
+        <div className="products-page__filters">
+          <p>trigo[earthbpuiaesrgtbpuewipubgtoihnsortino</p>
+        </div>
+        <div className="products-page__products">
+          {products.map((product) => {
+            return <SingleProduct key={product.id} {...product} />
+          })}
+        </div>
+      </div>
+    </section>
+  )
 }
 export default ProductsPage
