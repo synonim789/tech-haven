f9acf0b123d7e65d0316f02b8426f1d71c25a526
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-10-25 15:54:13 +0200

    Add Grid View

---
+++ frontend/tech-haven/src/components/GridView/GridView.jsx
@@ -0,0 +1,14 @@
+import { useFilterContext } from '../../context/filter_context'
+import SingleProduct from '../SingleProduct/SingleProduct'
+import './GridView.css'
+const GridView = () => {
+  const { pagedProducts: products } = useFilterContext()
+  return (
+    <div className="grid-view__products">
+      {products.map((product) => {
+        return <SingleProduct key={product.id} {...product} />
+      })}
+    </div>
+  )
+}
+export default GridView
