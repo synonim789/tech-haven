c36247fd92b27d93fc6281afe37cc8b8c1327987
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-10-25 15:53:53 +0200

    Add Sort

--- frontend/tech-haven/src/components/Sort/Sort.jsx
+++ frontend/tech-haven/src/components/Sort/Sort.jsx
@@ -1,16 +1,42 @@
 import { useFilterContext } from '../../context/filter_context'
+import { BsFillGridFill } from 'react-icons/bs'
+import { AiOutlineUnorderedList } from 'react-icons/ai'
 import './Sort.css'
 
 const Sort = () => {
-  const { filteredProducts, updateSort } = useFilterContext()
+  const {
+    filteredProducts,
+    updateSort,
+    setGridView,
+    setListView,
+    gridView,
+    listView,
+  } = useFilterContext()
   return (
     <div className="sort">
-      <select name="sort" className="sort-input" onChange={updateSort}>
-        <option value="name-asc">Name A-Z</option>
-        <option value="name-desc">Name Z-A</option>
-        <option value="price-asc">Price Ascending</option>
-        <option value="price-desc">Price Descending</option>
-      </select>
+      <div className="sort__left">
+        <select name="sort" className="sort-input" onChange={updateSort}>
+          <option value="name-asc">Name A-Z</option>
+          <option value="name-desc">Name Z-A</option>
+          <option value="price-asc">Price Ascending</option>
+          <option value="price-desc">Price Descending</option>
+        </select>
+        <div className="sort__views">
+          <button
+            className={`sort__grid ${gridView ? 'sort__grid--active' : ''}`}
+            onClick={setGridView}
+          >
+            <BsFillGridFill />
+          </button>
+          <button
+            className={`sort__list ${listView ? 'sort__grid--active' : ''}`}
+            onClick={setListView}
+          >
+            <AiOutlineUnorderedList />
+          </button>
+        </div>
+      </div>
+
       <p className="sort__products-found">
         {filteredProducts.length} Products Found
       </p>
