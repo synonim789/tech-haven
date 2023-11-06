099dfd87311f832eba06e3d0e3b89e1b3d0f077a
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-11-06 18:07:36 +0100

    add ContactInfo component

--- frontend/tech-haven/src/pages/ContactPage/ContactPage.jsx
+++ frontend/tech-haven/src/pages/ContactPage/ContactPage.jsx
@@ -1,14 +1,16 @@
 import ContactForm from '../../components/ContactForm/ContactForm'
+import ContactInfo from '../../components/ContactInfo/ContactInfo'
 import './ContactPage.css'
 
 const ContactPage = () => {
   return (
     <section className="contact-page">
-      <h1 className="contact-title">Contact</h1>
-      <div className="contact-info">
+      <h1 className="contact-page__title">Contact</h1>
+      <div className="contact-page__info">
         <ContactForm />
+        <ContactInfo />
       </div>
-      <div className="contact-map"></div>
+      <div className="contact-page__map"></div>
     </section>
   )
 }
