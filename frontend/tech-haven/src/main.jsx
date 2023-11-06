57e78dab7dd6a779b378a65574acae5dfad87f61
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-11-06 17:11:52 +0100

    add and style react toastify component

--- frontend/tech-haven/src/main.jsx
+++ frontend/tech-haven/src/main.jsx
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import App from './App.jsx'
 import { CartProvider } from './context/cart_context.jsx'
 import { FilterProvider } from './context/filter_context.jsx'
@@ -9,6 +11,19 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <ProductsProvider>
     <FilterProvider>
       <CartProvider>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+          toastClassName="toast-fontSize"
+        />
         <App />
       </CartProvider>
     </FilterProvider>
