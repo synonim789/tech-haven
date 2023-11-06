7da834622689030f1862660e5f43fdcf93f761a5
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-11-06 18:18:34 +0100

    add google map to contact page

--- frontend/tech-haven/src/pages/ContactPage/ContactPage.jsx
+++ frontend/tech-haven/src/pages/ContactPage/ContactPage.jsx
@@ -10,7 +10,15 @@ const ContactPage = () => {
         <ContactForm />
         <ContactInfo />
       </div>
-      <div className="contact-page__map"></div>
+      <div className="contact-page__map">
+        <iframe
+          width="925"
+          height="420"
+          frameBorder="0"
+          id="gmap_canvas"
+          src="https://maps.google.com/maps?width=925&amp;height=420&amp;hl=en&amp;q=4563%20Larwin%20Ave%20Cypress+(TechHaven)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        ></iframe>
+      </div>
     </section>
   )
 }
