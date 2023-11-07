bc979c2c846f2f87761cbb29fdd58ba3935174e3
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-11-07 18:21:33 +0100

    change logout button border size and radius and add profile icon

--- frontend/tech-haven/src/components/Header/Header.jsx
+++ frontend/tech-haven/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
+import { CgProfile } from 'react-icons/cg'
 import { Link, useLocation } from 'react-router-dom'
 import { useUserContext } from '../../context/UserContext'
 import './Header.css'
-
 const Header = () => {
   const { user, logoutUser } = useUserContext()
   const location = useLocation()
@@ -22,7 +22,10 @@ const Header = () => {
             <button onClick={logoutUser} className="header__logout">
               Log out
             </button>
-            <p className="header__username">{user.user}</p>
+            <p className="header__username">
+              {user.user}
+              <CgProfile />
+            </p>
           </div>
         )}
         {!user && (
