52ee94719c7276d036174629ce1f81a76d166c89
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-11-10 16:31:35 +0100

    add bem methology to profile page

--- frontend/tech-haven/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/tech-haven/src/pages/ProfilePage/ProfilePage.jsx
@@ -6,36 +6,52 @@ const ProfilePage = () => {
   return (
     <div className="profile-page">
       <div>
-        <h3>Name:</h3>
-        <p>{user.name ? user.name : 'Not set'}</p>
+        <h3 className="profile-info__title">Name:</h3>
+        <p className="profile-info__description">
+          {user.name ? user.name : 'Not set'}
+        </p>
       </div>
       <div>
-        <h3>Email:</h3>
-        <p>{user.email ? user.email : 'Not set'}</p>
+        <h3 className="profile-info__title">Email:</h3>
+        <p className="profile-info__description">
+          {user.email ? user.email : 'Not set'}
+        </p>
       </div>
       <div>
-        <h3>Phone:</h3>
-        <p>{user.phone ? user.phone : 'Not set'}</p>
+        <h3 className="profile-info__title">Phone:</h3>
+        <p className="profile-info__description">
+          {user.phone ? user.phone : 'Not set'}
+        </p>
       </div>
       <div>
-        <h3>Street:</h3>
-        <p>{user.street ? user.street : 'Not set'}</p>
+        <h3 className="profile-info__title">Street:</h3>
+        <p className="profile-info__description">
+          {user.street ? user.street : 'Not set'}
+        </p>
       </div>
       <div>
-        <h3>Apartment:</h3>
-        <p>{user.apartment ? user.apartment : 'Not set'}</p>
+        <h3 className="profile-info__title">Apartment:</h3>
+        <p className="profile-info__description">
+          {user.apartment ? user.apartment : 'Not set'}
+        </p>
       </div>
       <div>
-        <h3>City:</h3>
-        <p>{user.city ? user.city : 'Not set'}</p>
+        <h3 className="profile-info__title">City:</h3>
+        <p className="profile-info__description">
+          {user.city ? user.city : 'Not set'}
+        </p>
       </div>
       <div>
-        <h3>Zip:</h3>
-        <p>{user.zip ? user.zip : 'Not set'}</p>
+        <h3 className="profile-info__title">Zip:</h3>
+        <p className="profile-info__description">
+          {user.zip ? user.zip : 'Not set'}
+        </p>
       </div>
       <div>
-        <h3>Country:</h3>
-        <p>{user.country ? user.country : 'Not set'}</p>
+        <h3 className="profile-info__title">Country:</h3>
+        <p className="profile-info__description">
+          {user.country ? user.country : 'Not set'}
+        </p>
       </div>
     </div>
   )
