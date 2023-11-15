730ab7738c59e2fc5b8c25f4b63ec891d18b80db
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-11-15 17:55:56 +0100

    add DeleteUserModal

---
+++ frontend/tech-haven/src/components/DeleteUserModal/DeleteUserModal.jsx
@@ -0,0 +1,33 @@
+import { createPortal } from 'react-dom'
+import { useUserContext } from '../../context/UserContext'
+import './DeleteUserModal.css'
+const DeleteUserModal = ({ open, onClose }) => {
+  const { deleteUser, deleteUserError, deleteUserLoading } = useUserContext()
+
+  console.log(deleteUserError)
+
+  if (deleteUserLoading) {
+    return <div className="modal">Loading...</div>
+  }
+  if (!open) return null
+  return createPortal(
+    <>
+      <div className="modal__overlay" />
+      <div className="modal">
+        <h3 className="modal__title">
+          Do you really want to delete your account?
+        </h3>
+        <div className="modal__buttons">
+          <button className="modal__delete" onClick={deleteUser}>
+            Yes, I do
+          </button>
+          <button className="modal__close" onClick={onClose}>
+            No, I don&apos;t
+          </button>
+        </div>
+      </div>
+    </>,
+    document.getElementById('portal')
+  )
+}
+export default DeleteUserModal
--- frontend/tech-haven/src/pages/UserSettingsPage/UserSettingsPage.jsx
+++ frontend/tech-haven/src/pages/UserSettingsPage/UserSettingsPage.jsx
@@ -1,4 +1,27 @@
+import { useState } from 'react'
+import DeleteUserModal from '../../components/DeleteUserModal/DeleteUserModal'
+import './UserSettingsPage.css'
+
 const UserSettingsPage = () => {
-  return <div>UserSettingsPage</div>
+  const [modalOpen, setIsModalOpen] = useState(false)
+  return (
+    <main className="settings">
+      <h1 className="settings__title">Settings</h1>
+      <button
+        className="settings__delete"
+        onClick={() => {
+          setIsModalOpen(true)
+        }}
+      >
+        Delete Account
+      </button>
+      <DeleteUserModal
+        open={modalOpen}
+        onClose={() => {
+          setIsModalOpen(false)
+        }}
+      />
+    </main>
+  )
 }
 export default UserSettingsPage
