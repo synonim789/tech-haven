dd53ac2dd582986c3c1253dc14cd62bb021fe83a
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-01 18:39:32 +0100

    conver products page to tailwind

---
+++ frontend/tech-haven/src/pages/ProductsPage.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import Filters from '../components/Filters/Filters'
+import { Pagination } from '../components/Pagination/Pagination'
+import ProductsList from '../components/ProductsList/ProductsList'
+import SearchBar from '../components/SearchBar/SearchBar'
+import Sort from '../components/Sort/Sort'
+import { useFilterContext } from '../context/filter_context'
+
+const ProductsPage = () => {
+  const { clearFilters } = useFilterContext()!
+  useEffect(() => {
+    return function cleanup() {
+      clearFilters()
+    }
+  }, [])
+  return (
+    <section className="flex flex-col justify-center">
+      <div className="max-w-6xl mx-auto">
+        <SearchBar />
+        <Sort />
+        <div className="flex justify-between">
+          <Filters />
+          <ProductsList />
+        </div>
+        <Pagination />
+      </div>
+    </section>
+  )
+}
+export default ProductsPage
