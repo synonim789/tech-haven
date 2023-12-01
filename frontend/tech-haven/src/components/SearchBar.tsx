73d3e0c7fa18f178a9753b63bc69a3882cf7b9f1
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-01 19:21:55 +0100

    update searchbar width

--- frontend/tech-haven/src/components/SearchBar.tsx
+++ frontend/tech-haven/src/components/SearchBar.tsx
@@ -8,7 +8,7 @@ const SearchBar = () => {
       <input
         type="text"
         placeholder="Search..."
-        className="p-4 w-full overflow-hidden my-10 rounded-full text-2xl shadow-sm"
+        className="p-4 w-4/5 overflow-hidden my-10 rounded-full text-2xl shadow-sm"
         name="search"
         onChange={updateFilters}
         value={filters.search}
