ccd51912630f062a3d1c1b2692af844cb26b72e5
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-02 15:47:07 +0100

    convert NotFoundPage to Tailwind

---
+++ frontend/tech-haven/src/pages/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+const NotFoundPage = () => {
+  return (
+    <main className="flex items-center justify-center flex-col min-h-[80vh]">
+      <h1 className="text-red-500 text-9xl">404</h1>
+      <h2 className="text-4xl">Page Not Found</h2>
+      <Link
+        to="/"
+        className="bg-[#120b90] text-white px-4 py-2 rounded-lg shadow-lg text-2xl font-bold mt-5"
+      >
+        Back To Home
+      </Link>
+    </main>
+  )
+}
+export default NotFoundPage
