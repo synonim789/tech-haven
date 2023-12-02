000aa4834b02e2f3dd9cf5643bca9d6944c15c1b
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-02 15:18:47 +0100

    convert DeleteUserModal to tailwind

---
+++ frontend/tech-haven/src/components/DeleteUserModal.tsx
@@ -0,0 +1,48 @@
+import { createPortal } from 'react-dom'
+import { useAuthContext } from '../context/AuthContext'
+import { useUserContext } from '../context/UserContext'
+
+type DeleteUserModalPropsType = {
+  open: boolean
+  onClose: () => void
+}
+
+const DeleteUserModal = ({ open, onClose }: DeleteUserModalPropsType) => {
+  const { deleteUser, deleteUserLoading } = useUserContext()!
+  const { token } = useAuthContext()!
+
+  if (deleteUserLoading) {
+    return (
+      <div className="fixed top-[50%] left-[50%] -translate-y-1/2 -translate-x-1/2 bg-white z-50 p-14 flex flex-col items-center gap-14 rounded-3xl">
+        Loading...
+      </div>
+    )
+  }
+  if (!open) return null
+  return createPortal(
+    <>
+      <div className="fixed inset-0  z-40 bg-neutral-400 opacity-40" />
+      <div className="fixed top-[50%] left-[50%] -translate-y-1/2 -translate-x-1/2 bg-white z-50 p-14 flex flex-col items-center gap-14 rounded-3xl">
+        <h3 className="text-3xl">Do you really want to delete your account?</h3>
+        <div className="flex gap-14">
+          <button
+            className="px-4 py-2 text-[20px] font-bold text-white bg-green-700 rounded-xl hover:scale-105 hover:opacity-75"
+            onClick={() => {
+              deleteUser(token)
+            }}
+          >
+            Yes, I do
+          </button>
+          <button
+            className="px-4 py-2 text-[20px] font-bold text-white bg-red-500 rounded-xl hover:scale-105 hover:opacity-75"
+            onClick={onClose}
+          >
+            No, I don&apos;t
+          </button>
+        </div>
+      </div>
+    </>,
+    document.getElementById('portal')!
+  )
+}
+export default DeleteUserModal
--- frontend/tech-haven/src/pages/UserSettingsPage.tsx
+++ frontend/tech-haven/src/pages/UserSettingsPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import DeleteUserModal from '../components/DeleteUserModal/DeleteUserModal'
+import DeleteUserModal from '../components/DeleteUserModal'
 
 const UserSettingsPage = () => {
   const [modalOpen, setIsModalOpen] = useState(false)
