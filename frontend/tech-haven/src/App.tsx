98984ed6fc1d105420d7472723157d85baccfe71
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-02 17:26:02 +0100

    udate imports in App

--- frontend/tech-haven/src/App.tsx
+++ frontend/tech-haven/src/App.tsx
@@ -1,39 +1,39 @@
 import { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
-import FullscreenLoading from './components/FullscreenLoading/FullscreenLoading'
+import FullscreenLoading from './components/FullscreenLoading'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
 import { useAuthContext } from './context/AuthContext'
 import { useUserContext } from './context/UserContext'
 import ScrollToTop from './helpers/ScrollToTop'
-import AboutPage from './pages/AboutPage/AboutPage'
-import AddCategory from './pages/AddCategoryPage/AddCategory'
-import AddProductPage from './pages/AddProductPage/AddProductPage'
-import AddUserPage from './pages/AddUserPage/AddUserPage'
-import AdminPage from './pages/AdminPage/AdminPage'
-import AdminWelcomePage from './pages/AdminWelcomePage/AdminWelcomePage'
-import CartPage from './pages/CartPage/CartPage'
-import ContactPage from './pages/ContactPage/ContactPage'
-import EditCategoryPage from './pages/EditCategoryPage/EditCategoryPage'
-import EdictProductPage from './pages/EditProductPage/EdictProductPage'
-import ForgotPassword from './pages/ForgotPasswordPage/ForgotPassword'
+import AboutPage from './pages/AboutPage'
+import AddCategory from './pages/AddCategory'
+import AddProductPage from './pages/AddProductPage'
+import AddUserPage from './pages/AddUserPage'
+import AdminPage from './pages/AdminPage'
+import AdminWelcomePage from './pages/AdminWelcomePage'
+import CartPage from './pages/CartPage'
+import ContactPage from './pages/ContactPage'
+import EdictProductPage from './pages/EdictProductPage'
+import EditCategoryPage from './pages/EditCategoryPage'
+import ForgotPassword from './pages/ForgotPassword'
 import Homepage from './pages/HomePage'
-import LoginPage from './pages/LoginPage/LoginPage'
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
-import ProductPage from './pages/ProductPage/ProductPage'
+import LoginPage from './pages/LoginPage'
+import NotFoundPage from './pages/NotFoundPage'
+import ProductPage from './pages/ProductPage'
 import ProductsPage from './pages/ProductsPage'
-import ProfilePage from './pages/ProfilePage/ProfilePage'
-import RemoveCategoryPage from './pages/RemoveCategoryPage/RemoveCategoryPage'
-import RemoveProductPage from './pages/RemoveProductPage/RemoveProductPage'
-import RemoveUserPage from './pages/RemoveUserPage/RemoveUserPage'
-import SignUpPage from './pages/SignUpPage/SignUpPage'
-import StatisticsPage from './pages/StatisticsPage/StatisticsPage'
-import UserChangeInfo from './pages/UserChangeInfo/UserChangeInfo'
-import UserOrderPage from './pages/UserOrderPage/UserOrderPage'
-import UserProfileInfo from './pages/UserProfileInfo/UserProfileInfo'
-import UserSettingsPage from './pages/UserSettingsPage/UserSettingsPage'
-import UserWelcomePage from './pages/UserWelcomePage/UserWelcomePage'
+import ProfilePage from './pages/ProfilePage'
+import RemoveCategoryPage from './pages/RemoveCategoryPage'
+import RemoveProductPage from './pages/RemoveProductPage'
+import RemoveUserPage from './pages/RemoveUserPage'
+import SignUpPage from './pages/SignUpPage'
+import StatisticsPage from './pages/StatisticsPage'
+import UserChangeInfo from './pages/UserChangeInfo'
+import UserOrderPage from './pages/UserOrderPage'
+import UserProfileInfo from './pages/UserProfileInfo'
+import UserSettingsPage from './pages/UserSettingsPage'
+import UserWelcomePage from './pages/UserWelcomePage'
 import AdminRoute from './routes/AdminRoute'
 import AuthRoute from './routes/AuthRoute'
 import GuestRoute from './routes/GuestRoute'
@@ -117,7 +117,7 @@ function App() {
               path="products"
               element={
                 <div>
-                  <h1>Products</h1>
+                  <h1 className="text-6xl font-bold">Products</h1>
                 </div>
               }
             />
@@ -127,7 +127,7 @@ function App() {
               path="categories"
               element={
                 <div>
-                  <h1>Categories</h1>
+                  <h1 className="text-6xl font-bold">Categories</h1>
                 </div>
               }
             />
@@ -138,7 +138,7 @@ function App() {
               path="users"
               element={
                 <div>
-                  <h1>Users</h1>
+                  <h1 className="text-6xl font-bold">Users</h1>
                 </div>
               }
             />
