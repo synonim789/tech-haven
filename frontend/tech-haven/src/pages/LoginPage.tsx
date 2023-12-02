70674db30ec8484d8cdf3e92817b774c5a5eac83
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-02 13:42:35 +0100

    convert SignUpPage to tailwind

--- frontend/tech-haven/src/pages/LoginPage.tsx
+++ frontend/tech-haven/src/pages/LoginPage.tsx
@@ -6,8 +6,7 @@ import {
   AiOutlineEyeInvisible,
 } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
-import { useAuthContext } from '../../context/AuthContext'
-import './LoginPage.css'
+import { useAuthContext } from '../context/AuthContext'
 
 type LoginFormType = {
   email: string
@@ -26,25 +25,29 @@ const LoginPage = () => {
   const { register, handleSubmit, formState } = form
   const { errors } = formState
   return (
-    <div className="login-page">
-      <div className="login-page__container">
-        <div className="login-page__back">
-          <Link to="/">
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="bg-white p-10 flex flex-col justify-center items-center rounded-xl shadow-lg gap-10">
+        <div className="w-full text-[#120b90] font-bold">
+          <Link to="/" className="flex items-center text-[20px]">
             <AiOutlineArrowLeft />
             Back Home
           </Link>
         </div>
 
-        <h1 className="login-page__title">
-          <span>Log In</span> | <Link to="/sign-up">Sign Up</Link>
+        <h1 className="text-2xl font-bold text-slate-600">
+          <span className="text-[#120b90]">Log In</span> |{' '}
+          <Link to="/sign-up">Sign Up</Link>
         </h1>
-        <form className="login-page__form" onSubmit={handleSubmit(loginUser)}>
-          <label>
+        <form
+          className="flex flex-col w-full justify-center items-center gap-7"
+          onSubmit={handleSubmit(loginUser)}
+        >
+          <label className="flex flex-col w-full text-[20px] font-bold cursor-pointer">
             <span>Email</span>
             <input
               type="email"
               id="email"
-              className="login-page__input"
+              className="px-3 py-2 border-[2px] border-solid border-slate-300 shadow-lg rounded-xl"
               placeholder="Enter Email"
               {...register('email', {
                 required: 'Email is required',
@@ -55,17 +58,19 @@ const LoginPage = () => {
                 },
               })}
             />
-            <p className="login-page__input__error">{errors.email?.message}</p>
+            <p className="font-bold text-red-600 flex flex-col">
+              {errors.email?.message}
+            </p>
           </label>
-          <label>
+          <label className="flex flex-col w-full text-[20px] font-bold cursor-pointer">
             <span>Password</span>
-            <div className="login-page__password-container">
+            <div className="relative px-3 py-2  border-[2px] border-solid border-slate-300 shadow-lg rounded-xl flex justify-between">
               <input
                 type={visiblePassword ? 'text' : 'password'}
                 id="password"
                 autoComplete="on"
                 placeholder="Enter Password"
-                className="login-page__password-input"
+                className="h-[100%] text-[20px] outline-none"
                 {...register('password', {
                   required: 'Password is required',
                 })}
@@ -75,19 +80,29 @@ const LoginPage = () => {
                   setVisiblePassword(!visiblePassword)
                 }}
               >
-                {visiblePassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+                {visiblePassword ? (
+                  <AiOutlineEyeInvisible className="absolute top-[30%] right-[10px] cursor-pointer" />
+                ) : (
+                  <AiOutlineEye className="absolute top-[30%] right-[10px] cursor-pointer" />
+                )}
               </div>
             </div>
-            <p className="login-page__input__error">
+            <p className="font-bold text-red-600 flex flex-col">
               {errors.password?.message}
             </p>
           </label>
-          {error ? <p className="login-page__error">{error}</p> : null}
-          <button type="submit" className="login-page__cta">
+          {error ? <p className="font-bold text-red-600">{error}</p> : null}
+          <button
+            type="submit"
+            className="bg-[#120b90] text-white font-bold px-4 py-2 rounded-lg text-[24px] hover:scale-105 hover:opacity-80 transition"
+          >
             {loading ? 'Logging In...' : 'Log In'}
           </button>
         </form>
-        <Link to="/forgot-password" className="login-page__forgot-pass">
+        <Link
+          to="/forgot-password"
+          className="text-[#120b90] font-bold text-[20px]"
+        >
           Forgot Password?
         </Link>
       </div>
