01231c841c582057ef865978f5cb8afdf0ccf61c
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-05 19:40:08 +0100

    add padding to hero

--- frontend/tech-haven/src/components/Hero.tsx
+++ frontend/tech-haven/src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Hero = () => {
   return (
     <section className="min-h-[90vh] bg-hero bg-center bg-no-repeat bg-cover relative flex justify-center items-center">
-      <div className="max-w-5xl mx-auto flex flex-col justify-center items-center gap-16">
+      <div className="max-w-5xl mx-auto flex flex-col justify-center items-center gap-16 px-4">
         <h1 className="uppercase font-bold text-white text-6xl text-center relative after:content-[''] after:w-4/5 after:absolute after:h-2 after:bg-orange-500 after:-bottom-4 after:left-0 after:right-0 after:mx-auto">
           Make your life a tech haven
         </h1>
