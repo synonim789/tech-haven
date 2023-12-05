065fa748b6c6de27c5bee7ca7e49c45b487ea9c7
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-05 19:41:49 +0100

    add FormTextarea component

---
+++ frontend/tech-haven/src/components/FormTextarea.tsx
@@ -0,0 +1,23 @@
+type FormTextareaProps = {
+  name: string
+  type: string
+  error?: string | undefined
+  register: any
+}
+
+const FormTextarea = ({ name, error, register }) => {
+  return (
+    <div className="flex flex-col w-full text-[20px] font-semibold cursor-pointer capitalize my-4">
+      <label htmlFor={name}>{name}</label>
+      <textarea
+        name={name}
+        id={name}
+        className="px-3 py-2 border-[2px] border-solid border-slate-300 shadow-lg rounded-xl placeholder:capitalize resize-none h-[200px]"
+        placeholder={`Enter ${name}`}
+        {...register}
+      ></textarea>
+      <p className="font-bold text-red-600 flex flex-col">{error}</p>
+    </div>
+  )
+}
+export default FormTextarea
