ed7ea58ea7269af344ea14bbfe3e4af16a3bce24
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-05 19:42:31 +0100

    add margin to FormInput

--- frontend/tech-haven/src/components/FormInput.tsx
+++ frontend/tech-haven/src/components/FormInput.tsx
@@ -1,19 +1,19 @@
 type FormInputProps = {
   name: string
   type: string
-  error: string | undefined
+  error?: string | undefined
   register: any
 }
 
 const FormInput = ({ name, type, error, register }: FormInputProps) => {
   return (
-    <div className="flex flex-col w-full text-[20px] font-semibold cursor-pointer capitalize">
+    <div className="flex flex-col w-full text-[20px] font-semibold cursor-pointer capitalize my-2">
       <label htmlFor={name} id={name}>
         {name}
       </label>
       <input
         type={type}
-        className="px-3 py-2 border-[2px] border-solid border-slate-300 shadow-lg rounded-xl placeholder:capitalize"
+        className="px-3 py-2 border-[2px] border-solid border-slate-300 shadow-lg rounded-xl placeholder:capitalize "
         placeholder={`Enter ${name}`}
         {...register}
       />
