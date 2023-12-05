98b436199d512c92f22710bc2ac73bcb016ad437
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-05 18:27:30 +0100

    add FormButton component

---
+++ frontend/tech-haven/src/components/FormButton.tsx
@@ -0,0 +1,17 @@
+type FormButtonProps = {
+    loading: boolean
+    loadingText?: string
+    text: string
+}
+
+const FormButton = ({ loading, loadingText, text }: FormButtonProps) => {
+  return (
+    <button
+      type="submit"
+      className="bg-[#120b90] text-white font-bold px-4 py-2 rounded-lg text-[24px] hover:scale-105 hover:opacity-80 transition"
+    >
+      {loading ? loadingText : text}
+    </button>
+  )
+}
+export default FormButton
