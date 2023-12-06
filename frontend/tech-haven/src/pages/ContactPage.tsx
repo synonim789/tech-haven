07e54d160cde3404625cd4dce75dac0476cc30ed
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-06 14:53:24 +0100

    add ContactPage responsiveness

--- frontend/tech-haven/src/pages/ContactPage.tsx
+++ frontend/tech-haven/src/pages/ContactPage.tsx
@@ -7,17 +7,15 @@ const ContactPage = () => {
       <h1 className="font-bold text-5xl text-center text-[#120b90] relative after:content-[''] after:absolute after:bg-orange-500 after:w-4/5 after:h-1 after:-bottom-1 after:left-0 after:right-0 after:mx-auto">
         Contact
       </h1>
-      <div className="flex gap-5">
+      <div className="flex gap-5 flex-col md:flex-row px-4">
         <ContactForm />
         <ContactInfo />
       </div>
-      <div className="w-fit m-auto p-4 bg-white rounded-xl shadow-md">
+      <div className="w-fit m-auto p-4 bg-white rounded-xl shadow-md mt-4">
         <iframe
-          width="925"
-          height="420"
-          frameBorder="0"
           id="gmap_canvas"
           src="https://maps.google.com/maps?width=925&amp;height=420&amp;hl=en&amp;q=4563%20Larwin%20Ave%20Cypress+(TechHaven)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          className="w-fit h-full md:w-[700px] md:h-[300px]"
         ></iframe>
       </div>
     </section>
