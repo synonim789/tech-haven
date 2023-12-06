891c56b172c1ebc1e1fa528bd2ac5e48e097ea38
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-06 14:51:20 +0100

    add contact form responsiveness

--- frontend/tech-haven/src/components/ContactInfo.tsx
+++ frontend/tech-haven/src/components/ContactInfo.tsx
@@ -3,18 +3,26 @@ import { HiOutlineMapPin } from 'react-icons/hi2'
 
 const ContactInfo = () => {
   return (
-    <section className="flex flex-col items-center justify-center gap-5">
-      <p className="text-[25px] font-bold flex items-center gap-2">
-        <AiOutlinePhone />
-        Email: <span className="text-[#120b90]">techhaven@techaven.com</span>
+    <section className="flex flex-col items-center justify-center gap-5 break-all ">
+      <p className="text-[20px] font-bold flex items-center gap-2 md:text-[25px] flex-col">
+        <span className="flex flex-row items-center">
+          <AiOutlinePhone /> Phone:
+        </span>
+        <span className="text-[#120b90]">techhaven@techaven.com</span>
       </p>
-      <p className="text-[25px] font-bold flex items-center gap-2">
-        <AiOutlineMail />
-        Phone: <span className="text-[#120b90]">209-832-3434</span>
+      <p className="text-[20px] font-bold flex items-center gap-2 md:text-[25px] flex-col ">
+        <span className="flex flex-row items-center">
+          <AiOutlineMail />
+          Email:
+        </span>
+        <span className="text-[#120b90]">209-832-3434</span>
       </p>
-      <p className="text-[25px] font-bold flex items-center gap-2">
-        <HiOutlineMapPin />
-        Address:{' '}
+      <p className="text-[20px] font-bold flex items-center gap-2 md:text-[25px] flex-col">
+        <span className="flex flex-row items-center">
+          <HiOutlineMapPin />
+          Address:
+        </span>
+
         <span className="text-[#120b90]">
           4563 Larwin Ave, Cypress, California, US
         </span>
