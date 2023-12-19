660b5ee4a7e3f0722e52cd86419a3e0dc80d5e25
synonim789 <75422461+synonim789@users.noreply.github.com>
2023-12-19 10:38:10 +0100

    setup react query

--- frontend/tech-haven/src/main.tsx
+++ frontend/tech-haven/src/main.tsx
@@ -1,40 +1,45 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ReactDOM from 'react-dom/client'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import App from './App.tsx'
 import { AdminProvider } from './context/AdminContext'
 import { AuthProvider } from './context/AuthContext'
+import { UserProvider } from './context/UserContext.jsx'
 import { CartProvider } from './context/cart_context.jsx'
 import { FilterProvider } from './context/filter_context.jsx'
 import { ProductsProvider } from './context/products_context.jsx'
-import { UserProvider } from './context/UserContext.jsx'
 import './index.css'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <AuthProvider>
-    <UserProvider>
-      <ProductsProvider>
-        <FilterProvider>
-          <CartProvider>
-            <ToastContainer
-              position="top-center"
-              autoClose={1000}
-              hideProgressBar={true}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="colored"
-              toastClassName="toast-fontSize"
-            />
-            <AdminProvider>
-              <App />
-            </AdminProvider>
-          </CartProvider>
-        </FilterProvider>
-      </ProductsProvider>
-    </UserProvider>
-  </AuthProvider>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <UserProvider>
+        <ProductsProvider>
+          <FilterProvider>
+            <CartProvider>
+              <ToastContainer
+                position="top-center"
+                autoClose={1000}
+                hideProgressBar={true}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+                toastClassName="toast-fontSize"
+              />
+              <AdminProvider>
+                <App />
+              </AdminProvider>
+            </CartProvider>
+          </FilterProvider>
+        </ProductsProvider>
+      </UserProvider>
+    </AuthProvider>
+  </QueryClientProvider>
 )
