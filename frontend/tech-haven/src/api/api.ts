b7f20a39b27237522ca430b07d2eb5e5a90939ce
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-01-02 12:43:35 +0100

    add header Authorization token  to fetchBaseQuery

--- frontend/tech-haven/src/api/api.ts
+++ frontend/tech-haven/src/api/api.ts
@@ -1,7 +1,18 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { RootState } from '../store'
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'http://localhost:3000/api/v1/',
+  prepareHeaders(headers, { getState }) {
+    const token = (getState() as RootState).auth.token
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`)
+    }
+  },
+})
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/v1/' }),
+  baseQuery,
   endpoints: () => ({}),
 })
