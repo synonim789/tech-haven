3248a0f28ef0cb2e0a428da63fda0491445a1820
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-01-17 19:34:00 +0100

    build: add UserOrder feature

--- frontend/tech-haven/src/features/orders/ordersApiSlice.ts
+++ frontend/tech-haven/src/features/orders/ordersApiSlice.ts
@@ -1,5 +1,23 @@
 import { api } from '../../api/api'
 
+type GetOrders = {
+  total: number
+  userOrders: OrderType[]
+}
+
+type OrderType = {
+  _id: string
+  orderItems: {}
+  shippingAddress1: string
+  shippingAddress2: string
+  phone: string
+  status: string
+  total: number
+  user: string
+  subtotal: number
+  dateOrdered: string
+}
+
 const ordersApiSlice = api.injectEndpoints({
   endpoints: (builder) => ({
     postOrder: builder.mutation({
@@ -9,7 +27,15 @@ const ordersApiSlice = api.injectEndpoints({
         body: { order },
       }),
     }),
+    getUserOrder: builder.query<
+      GetOrders,
+      { id: string | undefined; page: number }
+    >({
+      query: ({ id, page = 0 }) => ({
+        url: `users/orders/${id}?page=${page}`,
+      }),
+    }),
   }),
 })
 
-export const { usePostOrderMutation } = ordersApiSlice
+export const { usePostOrderMutation, useGetUserOrderQuery } = ordersApiSlice
