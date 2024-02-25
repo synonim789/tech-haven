ee59b3948f6900fd7a5bf4ad88528de4313913bd
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-02-25 19:11:51 +0100

    refactor deocde Token function

--- frontend/tech-haven/src/utils/decodeToken.tsx
+++ frontend/tech-haven/src/utils/decodeToken.tsx
@@ -7,9 +7,16 @@ type decodedToken = {
 }
 
 export const decodeToken = (token: string | null) => {
-  if (token) {
+  if (!token) {
+    return null
+  }
+
+  try {
     const decodedToken: decodedToken = jwtDecode(token)
     const { userId, role, exp } = decodedToken
     return { userId, role, exp }
+  } catch (error) {
+    console.error('Error decoding token:', error)
+    return null
   }
 }
