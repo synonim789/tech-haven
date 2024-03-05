25bba558d9b260bb3f6fe60b67f10b3fe8f496a7
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-03-05 19:56:08 +0100

    add PUT request to api

--- frontend/tech-haven/src/features/adminOrder/adminOrderApiSlice.ts
+++ frontend/tech-haven/src/features/adminOrder/adminOrderApiSlice.ts
@@ -1,12 +1,25 @@
 import { api } from '../../api/api'
 import { OrderType } from '../../types'
 
+type EditOrder = {
+  id: string
+  status: string
+}
+
 const adminOrderApiSlice = api.injectEndpoints({
   endpoints: (builder) => ({
     getAdminOrders: builder.query<OrderType[], void>({
       query: () => '/orders',
     }),
+    editOrder: builder.mutation<OrderType, EditOrder>({
+      query: ({ id, status }) => ({
+        url: `/orders/${id}`,
+        method: 'PUT',
+        body: { status },
+      }),
+    }),
   }),
 })
 
-export const { useGetAdminOrdersQuery } = adminOrderApiSlice
+export const { useGetAdminOrdersQuery, useEditOrderMutation } =
+  adminOrderApiSlice
