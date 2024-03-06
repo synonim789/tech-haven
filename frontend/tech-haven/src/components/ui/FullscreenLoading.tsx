59025223c39deae899e3c13bc918ac5c5e810c81
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-03-06 20:58:57 +0100

    change z index FullScreen Loading

--- frontend/tech-haven/src/components/ui/FullscreenLoading.tsx
+++ frontend/tech-haven/src/components/ui/FullscreenLoading.tsx
@@ -3,7 +3,7 @@ import { createPortal } from 'react-dom'
 const FullscreenLoading = () => {
   return createPortal(
     <>
-      <div className="bg-[#405684] fixed inset-0 z-40"></div>
+      <div className="bg-[#405684] fixed inset-0 z-2000"></div>
       <div className="w-[150px] h-[150px] border-[10px] border-solid border-white border-b-[transparent] rounded-full z-50 fixed top-1/2 left-1/2 mt-[-75px] ml-[-75px] animate-spin"></div>
     </>,
     document.getElementById('portal')!
