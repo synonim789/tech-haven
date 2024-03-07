0c15619103fc95a72907408000fd24a751d6826d
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-03-07 20:34:55 +0100

    add shadows on settings button and change color of update account info

--- frontend/tech-haven/src/pages/UserSettings/UserSettingsPage.tsx
+++ frontend/tech-haven/src/pages/UserSettings/UserSettingsPage.tsx
@@ -8,7 +8,7 @@ const UserSettingsPage = () => {
     <main className="flex items-start flex-col gap-8 my-8">
       <h3 className="text-3xl font-semibold dark:text-slate-500">Settings</h3>
       <button
-        className="bg-red-500 text-2xl text-white font-semibold py-2 px-4 rounded-lg hover:opacity-70 transition-all hover:scale-105"
+        className="bg-red-500 text-2xl text-white font-semibold py-2 px-4 rounded-lg hover:opacity-70 transition-all hover:scale-105 shadow-md"
         onClick={() => {
           setIsModalOpen(true)
         }}
@@ -26,7 +26,7 @@ const UserSettingsPage = () => {
       </h3>
       <Link
         to="update"
-        className="bg-[#182b90] text-2xl text-white font-semibold py-2 px-4 rounded-lg hover:opacity-70 transition-all hover:scale-105"
+        className="bg-[#405684] text-2xl text-white font-semibold py-2 px-4 rounded-lg hover:opacity-70 transition-all hover:scale-105 shadow-md"
       >
         Update Account Info
       </Link>
