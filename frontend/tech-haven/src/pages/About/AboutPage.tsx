2e8bac86dc01c44940d31a42c839630414d1bc50
synonim789 <synonim789@wp.pl>
2024-03-10 15:59:33 +0100

    refacto AboutPage to have different style

--- frontend/tech-haven/src/pages/About/AboutPage.tsx
+++ frontend/tech-haven/src/pages/About/AboutPage.tsx
@@ -3,24 +3,24 @@ import AboutImage from './AboutImage'
 
 const AboutPage = () => {
   return (
-    <main className="max-w-5xl mx-auto flex flex-col md:flex-row my-10 justify-between gap-20 items-center">
-      <div className="flex flex-col items-center gap-5 w-3/5 ">
-        <h2 className="text-5xl w-fit text-[#405684] font-bold text-center relative after:content-[''] after:absolute after:h-1 after:w-4/5 after:bg-orange-500 after:left-0 after:-bottom-1 after:right-0 after:mx-auto">
+    <section className="max-w-6xl mx-auto my-10 ">
+      <section className="px-4">
+        <h2 className="text-5xl fit text-[#405684] font-bold text-center relative after:content-[''] after:absolute after:h-1 after:w-[175px] after:bg-orange-500 after:left-0 after:-bottom-1 after:right-0 after:mx-auto mb-8 w-full">
           About us
         </h2>
-        <p className="text-2xl text-center dark:text-slate-400">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ab
-          aliquam quod? Architecto adipisci neque eaque necessitatibus ducimus
-          quod molestiae, minima quis repudiandae sint ex eligendi illo ratione
-          libero consectetur! Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Sequi, magnam tenetur! Veritatis corrupti voluptatum saepe
-          aspernatur.
-        </p>
-      </div>
-      <div>
-        <AboutImage src={image} />
-      </div>
-    </main>
+        <div className="w-full flex justify-between gap-5 items-center flex-col-reverse md:flex-row">
+          <p className="text-2xl text-center dark:text-slate-400 max-w-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ab
+            aliquam quod? Architecto adipisci neque eaque necessitatibus ducimus
+            quod molestiae, minima quis repudiandae sint ex eligendi illo
+            ratione libero consectetur! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sequi, magnam tenetur! Veritatis corrupti
+            voluptatum saepe aspernatur.
+          </p>
+          <AboutImage src={image} />
+        </div>
+      </section>
+    </section>
   )
 }
 export default AboutPage
