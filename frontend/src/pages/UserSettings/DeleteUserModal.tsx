e0f67bb95bb094f70dbf95b5c59c5341aae2594c
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-03-14 20:43:49 +0100

    Add FocusTrap to DeleteUserModal

--- frontend/src/pages/UserSettings/DeleteUserModal.tsx
+++ frontend/src/pages/UserSettings/DeleteUserModal.tsx
@@ -1,3 +1,4 @@
+import FocusTrap from 'focus-trap-react'
 import { createPortal } from 'react-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { logout } from '../../features/auth/authSlice'
@@ -34,27 +35,29 @@ const DeleteUserModal = ({ open, onClose }: Props) => {
   return createPortal(
     <>
       <div className="fixed inset-0  z-40 bg-neutral-400 opacity-40" />
-      <div className="fixed top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 bg-white z-50 p-8 md:p-14 flex flex-col items-center gap-4 md:gap-8 rounded-3xl dark:bg-[#121212] dark:text-slate-400">
-        <h3 className="text-2xl md:text-3xl font-bold text-center">
-          Do you really want to delete your account?
-        </h3>
-        <div className="flex flex-col md:flex-row w-full justify-center items-center gap-4 md:gap-8">
-          <button
-            className="px-4 py-2 text-[20px] font-bold text-white bg-green-700 rounded-xl hover:scale-105 hover:opacity-75"
-            onClick={() => {
-              deleteHandler()
-            }}
-          >
-            Yes, I do
-          </button>
-          <button
-            className="px-4 py-2 text-[20px] font-bold text-white bg-red-500 rounded-xl hover:scale-105 hover:opacity-75"
-            onClick={onClose}
-          >
-            No, I don&apos;t
-          </button>
-        </div>
-      </div>
+      <FocusTrap>
+        <dialog className="fixed top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 bg-white z-50 p-8 md:p-14 flex flex-col items-center gap-4 md:gap-8 rounded-3xl dark:bg-[#121212] dark:text-slate-400">
+          <h3 className="text-2xl md:text-3xl font-bold text-center">
+            Do you really want to delete your account?
+          </h3>
+          <div className="flex flex-col md:flex-row w-full justify-center items-center gap-4 md:gap-8">
+            <button
+              className="px-4 py-2 text-[20px] font-bold text-white bg-green-700 rounded-xl hover:scale-105 hover:opacity-75"
+              onClick={() => {
+                deleteHandler()
+              }}
+            >
+              Yes, I do
+            </button>
+            <button
+              className="px-4 py-2 text-[20px] font-bold text-white bg-red-500 rounded-xl hover:scale-105 hover:opacity-75"
+              onClick={onClose}
+            >
+              No, I don&apos;t
+            </button>
+          </div>
+        </dialog>
+      </FocusTrap>
     </>,
     document.getElementById('portal')!
   )
