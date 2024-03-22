37076011686d09f969e3b3afeaeeb7be9ae313c1
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-03-22 17:46:57 +0100

    Refactor statisticController.ts to use RequestHandler from express

--- server/controllers/statisticController.ts
+++ server/controllers/statisticController.ts
@@ -1,132 +1,161 @@
-import { Request, Response } from "express";
+import { RequestHandler } from "express";
+import * as createHttpError from "http-errors";
 import Order from "../models/order";
 import Product from "../models/product";
 import User from "../models/user";
 
-export const getOrderCount = async (req: Request, res: Response) => {
-  const orderCount = await Order.countDocuments()
-    .then((count) => count)
-    .catch((err) => {
-      return res.status(400).json({ success: false, error: err });
+export const getOrderCount: RequestHandler = async (req, res, next) => {
+  try {
+    const orderCount = await Order.countDocuments()
+      .then((count) => count)
+      .catch((err) => {
+        throw createHttpError(500, err.toString());
+      });
+    if (!orderCount) {
+      throw createHttpError(404, "Orders not found");
+    }
+    res.status(200).json({
+      count: orderCount,
     });
-  if (!orderCount) {
-    return res.status(500).json({ success: false });
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json({
-    count: orderCount,
-  });
 };
 
-export const getTotalSales = async (req: Request, res: Response) => {
-  const totalSales = await Order.aggregate([
-    { $group: { _id: null, totalSales: { $sum: "$total" } } },
-  ]);
+export const getTotalSales: RequestHandler = async (req, res, next) => {
+  try {
+    const totalSales = await Order.aggregate([
+      { $group: { _id: null, totalSales: { $sum: "$total" } } },
+    ]);
 
-  if (!totalSales) {
-    return res
-      .status(400)
-      .json({ message: "The order sales cannot be generated" });
+    if (!totalSales) {
+      throw createHttpError(500, "Total sales cannot be generated");
+    }
+    res.status(200).json({ totalSales: totalSales.pop().totalSales });
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json({ totalSales: totalSales.pop().totalSales });
 };
 
-export const getProductsCount = async (req: Request, res: Response) => {
-  const productCount = await Product.countDocuments({ deleted: false })
-    .then((count) => count)
-    .catch((err) => {
-      return res.status(400).json({ success: false, error: err });
+export const getProductsCount: RequestHandler = async (req, res, next) => {
+  try {
+    const productCount = await Product.countDocuments({ deleted: false })
+      .then((count) => count)
+      .catch((err) => {
+        throw createHttpError(500, err.toString());
+      });
+    if (!productCount) {
+      throw createHttpError(404, "Product count not found");
+    }
+    res.status(200).json({
+      count: productCount,
     });
-  if (!productCount) {
-    return res.status(500).json({ success: false });
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json({
-    count: productCount,
-  });
 };
 
-export const getUserCount = async (req: Request, res: Response) => {
-  const userCount = await User.countDocuments({ deleted: false })
-    .then((count) => count)
-    .catch((err) => {
-      return res
-        .status(400)
-        .json({ message: "Something went wrong", err: err });
+export const getUserCount: RequestHandler = async (req, res, next) => {
+  try {
+    const userCount = await User.countDocuments({ deleted: false })
+      .then((count) => count)
+      .catch((err) => {
+        throw createHttpError(500, err.toString());
+      });
+
+    if (!userCount) {
+      throw createHttpError(404, "Users not found");
+    }
+    res.status(200).json({
+      count: userCount,
     });
-  if (!userCount) {
-    return res.status(500).json({ message: "users not found" });
+  } catch (error) {
+    next(error);
   }
-  res.json({
-    count: userCount,
-  });
 };
 
-export const getSalesByDate = async (req: Request, res: Response) => {
-  const salesByDate = await Order.aggregate([
-    {
-      $match: {
-        status: { $not: { $eq: "canceled" } },
+export const getSalesByDate: RequestHandler = async (req, res, next) => {
+  try {
+    const salesByDate = await Order.aggregate([
+      {
+        $match: {
+          status: { $not: { $eq: "canceled" } },
+        },
       },
-    },
-    {
-      $group: {
-        _id: {
-          $dateToString: {
-            format: "%Y-%m-%d",
-            date: "$dateOrdered",
+      {
+        $group: {
+          _id: {
+            $dateToString: {
+              format: "%Y-%m-%d",
+              date: "$dateOrdered",
+            },
+          },
+          total: {
+            $sum: "$total",
           },
         },
-        total: {
-          $sum: "$total",
-        },
       },
-    },
-    {
-      $sort: { _id: -1 },
-    },
-  ]);
-  res.status(200).json(salesByDate);
+      {
+        $sort: { _id: -1 },
+      },
+    ]);
+
+    if (!salesByDate) {
+      throw createHttpError(500, "Sales by Date couldn't be generated");
+    }
+
+    return res.status(200).json(salesByDate);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const getSalesByCategory = async (req: Request, res: Response) => {
-  const ordersByCategory = await Order.aggregate([
-    {
-      $match: {
-        status: { $not: { $eq: "canceled" } },
+export const getSalesByCategory: RequestHandler = async (req, res, next) => {
+  try {
+    const ordersByCategory = await Order.aggregate([
+      {
+        $match: {
+          status: { $not: { $eq: "canceled" } },
+        },
       },
-    },
-    {
-      $unwind: "$orderItems",
-    },
-    {
-      $lookup: {
-        from: "products",
-        localField: "orderItems.productId",
-        foreignField: "_id",
-        as: "product",
+      {
+        $unwind: "$orderItems",
       },
-    },
-    {
-      $unwind: "$product",
-    },
-    {
-      $lookup: {
-        from: "categories",
-        localField: "product.category",
-        foreignField: "_id",
-        as: "category",
+      {
+        $lookup: {
+          from: "products",
+          localField: "orderItems.productId",
+          foreignField: "_id",
+          as: "product",
+        },
       },
-    },
-    {
-      $unwind: "$category",
-    },
-    {
-      $group: {
-        _id: "$category._id",
-        categoryName: { $first: "$category.name" },
-        totalSales: { $sum: "$orderItems.price" },
+      {
+        $unwind: "$product",
       },
-    },
-  ]);
-
-  res.status(200).json(ordersByCategory);
+      {
+        $lookup: {
+          from: "categories",
+          localField: "product.category",
+          foreignField: "_id",
+          as: "category",
+        },
+      },
+      {
+        $unwind: "$category",
+      },
+      {
+        $group: {
+          _id: "$category._id",
+          categoryName: { $first: "$category.name" },
+          totalSales: { $sum: "$orderItems.price" },
+        },
+      },
+    ]);
+    if (!ordersByCategory) {
+      throw createHttpError(500, "orders By Category couldn't be generated");
+    }
+    res.status(200).json(ordersByCategory);
+  } catch (error) {
+    next(error);
+  }
 };
