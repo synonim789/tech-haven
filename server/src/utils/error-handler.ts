a73368bca1f59431c593f737a89e38aa69a6361e
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-07-08 19:30:29 +0200

    add error handling for zod valdiation errors

--- server/src/utils/error-handler.ts
+++ server/src/utils/error-handler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { isHttpError } from "http-errors";
+import { ZodError } from "zod";
 
 const errorHandler = (
   error: unknown,
@@ -13,6 +14,13 @@ const errorHandler = (
   if (isHttpError(error)) {
     statusCode = error.status;
     errorMessage = error.message;
+  }
+
+  if (error instanceof ZodError) {
+    const errorMessages = error.issues.map((issue) => issue.message);
+    const uniqueMessages = [...new Set(errorMessages)];
+    errorMessage = uniqueMessages.join(", ");
+    statusCode = 400;
   }
   res.status(statusCode).json({ message: errorMessage });
 };
