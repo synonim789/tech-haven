b333b7364bfc26cf5540621df83bda2b1b151d2e
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-07-09 17:09:27 +0200

    add env for port

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,18 @@
 import "dotenv/config";
 import mongoose from "mongoose";
-import env from "./utils/validateEnv";
 import app from "./app";
+import env from "./utils/validateEnv";
 
+const port = process.env.PORT || 3000;
 
 mongoose
   .connect(env.MONGODB_CONNECTION_STRING)
   .then(() => {
     console.log("Database Connection is ready...");
-    app.listen(3000, () => {
-      console.log("server is running now http://localhost:3000");
+    app.listen(port, () => {
+      console.log(`server started at port ${port}`);
     });
   })
   .catch((err) => {
     console.log(err);
   });
-
-
