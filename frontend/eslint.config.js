4f2108208bac1324afce575ee3d4068020edc3f0
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-08-06 13:16:18 +0200

    fix eslint build not working

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -9,8 +9,8 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...tailwind.configs['flat/recommended'],
   pluginReact.configs.flat.recommended,
+  ...tailwind.configs['flat/recommended'],
   {
     rules: {
       'react/react-in-jsx-scope': 'off',
