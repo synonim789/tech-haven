052d8f95bd918ff404c300b0a2c3f790a99c5493
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-08-26 16:15:09 +0200

    install prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,20 +1,30 @@
-import pluginJs from '@eslint/js'
-import pluginReact from 'eslint-plugin-react'
-import tailwind from 'eslint-plugin-tailwindcss'
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  ...tailwind.configs['flat/recommended'],
+export default tseslint.config(
   {
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-    },
+    ignores: ['dist']
   },
-]
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh
+    },
+        rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  }
+)
