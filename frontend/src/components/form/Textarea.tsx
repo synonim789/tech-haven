ec185bd89318720b8537649c60a4ac8d69032040
synonim789 <75422461+synonim789@users.noreply.github.com>
2024-08-27 18:17:26 +0200

    change placeholder color on error in textarea

--- frontend/src/components/form/Textarea.tsx
+++ frontend/src/components/form/Textarea.tsx
@@ -12,7 +12,7 @@ const Textarea = forwardRef<HTMLTextAreaElement, Props>(
         className={cn(
           'h-[200px] resize-none rounded-xl border-2 border-solid border-gray-300 px-3 py-2 shadow-lg outline-none placeholder:capitalize placeholder:text-slate-500 dark:border-gray-700 dark:bg-transparent dark:text-gray-400 w-full text-[20px] font-semibold',
           {
-            '!border-red-400': error,
+            '!border-red-400 !placeholder-red-400': error,
           },
           className,
         )}
